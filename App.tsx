461ec50319d9973914e115524565006f5fa9e7cc
d-dlzndi <d.dlzndi@gmail.com>
2022-07-14 00:12:07 +0900

    220714 1~2장 완료

--- App.tsx
+++ App.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TextInput,
+} from 'react-native';
 import * as D from './src/data';
 
-const person = D.createRandomPerson();
+import ClassComponent from './src/screens/ClassComponent';
+import ArrowComponent from './src/screens/ArrowComponent';
+import Person from './src/screens/Person';
+
+const people = D.makeArray(100).map(D.createRandomPerson);
 
 export default function App() {
+  const children = people.map(person => (
+    <Person key={person.id} person={person} />
+  ));
   return (
     <SafeAreaView>
-      <Text>{JSON.stringify(person, null, 2)}</Text>
+      <Button
+        title="안녕해요"
+        color="blue"
+        onPress={() => console.log('안녕해요~')}
+      />
+      <Button
+        title="놀라워요"
+        color="red"
+        onPress={() => Alert.alert('놀라워요~', '세상에나!')}
+      />
+      <TextInput
+        placeholder="enter your name"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+      />
+      <ScrollView>{children}</ScrollView>
     </SafeAreaView>
   );
 }
